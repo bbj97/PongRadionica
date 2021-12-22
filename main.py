dc866609418fd34d4c59613efacc2354a8714b73
blj97 <korisnickabljeonce@gmail.com>
2021-12-22 23:57:13 +0100

    Animacija za protivnika izdvojena u funkciji

--- main.py
+++ main.py
@@ -29,6 +29,23 @@ def playerAnimation():
     if player.bottom >= screenHeight:
         player.bottom = screenHeight
 
+def opponentAnimation():
+    global opponentSpeed
+    # opponent.y += opponentSpeed
+
+    # Granice terena za protivnika
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screenHeight:
+        opponent.bottom = screenHeight
+    
+    # Kretanje uz loptu
+    if opponent.top < ball.y:
+        opponent.top += opponentSpeed
+    if opponent.bottom > ball.y:
+        opponent.top -= opponentSpeed
+    
+
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -52,8 +69,11 @@ pygame.display.set_caption("Pong game")
 
 # Brzine lopte po osi
 ballSpeedX = 7
-ballSpeedY = 7
+ballSpeedY = -7
 playerSpeed = 0 # prvo vrijednost 0, objekt se ne kreće, zatim vrijednost 7
+opponentSpeed = 7 
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -81,6 +101,8 @@ while True:
     # Funckije za animaciju
     ballAnimation()
     playerAnimation()
+    opponentAnimation()
+    
 
     pygame.display.flip()
     clock.tick(60)
