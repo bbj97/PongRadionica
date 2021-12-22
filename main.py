a919ac68458d6d8ccedcedab0e13ee717e447d0b
blj97 <korisnickabljeonce@gmail.com>
2021-12-22 21:46:05 +0100

    kreiranje lopte, igraca i protivnika

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame, sys
 
 pygame.init()
-
 clock = pygame.time.Clock()
 
 screenWidth = 1024
 screenHeight = 768
 
 screen = pygame.display.set_mode((screenWidth,screenHeight))
+
+# Kreiranje lopte, igrača i protivnika
+ball = pygame.Rect(screenWidth/2-15, screenHeight/2-15, 30, 30)
+player = pygame.Rect(screenWidth-10, screenHeight/2-70, 10, 140)
+opponent = pygame.Rect(10, screenHeight/2-70, 10, 140)
 
 pygame.display.set_caption("Pong game")
 
@@ -19,4 +23,3 @@ while True:
 
     pygame.display.flip()
     clock.tick(60)
-    
