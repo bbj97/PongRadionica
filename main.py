265bbba72d135d7cb995d9a7ddd0914277ff5bd4
blj97 <korisnickabljeonce@gmail.com>
2021-12-22 22:41:16 +0100

    Izdvajanje animacije za loptu u funkciju, Global varijable

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import pygame, sys
 
+def ballAnimation():
+    global ballSpeedX, ballSpeedY
+
+    # Animacija kretanja lopte
+    ball.x += ballSpeedX
+    ball.y += ballSpeedY
+
+    # Postavljanje granica terena za loptu
+    if ball.top <= 0 or ball.bottom >= screenHeight:
+        ballSpeedY *= -1
+    if ball.left <= 0 or ball.right >= screenWidth:
+        ballSpeedX *= -1
+
+    # Sudaranje lopte sa igracem i s protivnikom
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        # ballSpeedX = -1 * ballSpeedX
+        ballSpeedX *= -1
+    
+
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -30,28 +49,15 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # Animacija kretanja lopte
-    ball.x += ballSpeedX
-    ball.y += ballSpeedY
-
-    # Postavljanje granica terena za loptu
-    if ball.top <= 0 or ball.bottom >= screenHeight:
-        ballSpeedY *= -1
-    if ball.left <= 0 or ball.right >= screenWidth:
-        ballSpeedX *= -1
-
-    # Sudaranje lopte sa igracem i s protivnikom
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        # ballSpeedX = -1 * ballSpeedX
-        ballSpeedX *= -1
-    
-    # vizualizacije
+    # Iscrtavanje objekata
     screen.fill(bgColor)
     pygame.draw.rect(screen, lightGrey, player)
     pygame.draw.rect(screen, lightGrey, opponent)
     pygame.draw.ellipse(screen, lightGrey, ball)
     pygame.draw.aaline(screen, lightGrey, (screenWidth/2,0), (screenWidth/2,screenHeight))
 
+    # Metoda za animaciju
+    ballAnimation()
 
     pygame.display.flip()
     clock.tick(60)
